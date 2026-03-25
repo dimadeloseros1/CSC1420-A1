63f7368f9be0052e1c53e13d2667c7c5d3bbabc7
dimadeloseros1 <dimadeloseros1@gmail.com>
2026-03-25 23:01:09 +1000

    string reversed using 'stack'

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+// void userInput();
+
+int main() {
+    // userInput();
+}
+//
+// void userInput() {
+//     string userInput;
+//
+//     cout << "Please enter an input of your choice: ";
+//     stack<char> st;
+//     // stack<string> temp;
+//
+//     while (getline(cin, userInput)) {
+//         cout << userInput << endl;
+//         break;
+//     }
+//
+//     for (char c : userInput) {
+//         st.push(c);
+//     }
+//
+//     string reversedStr;
+//
+//     while (!st.empty()) {
+//         reversedStr += st.top();
+//         st.pop();
+//     }
+//
+//     cout << reversedStr;
+//
+//
+//
+//     // cout << userResult.top();
+// }
