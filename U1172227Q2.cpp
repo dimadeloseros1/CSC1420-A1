dd74f4678d67806c68d374625d92df778212c290
dimadeloseros1 <dimadeloseros1@gmail.com>
2026-03-27 14:18:28 +1000

    Feat: Analyze the time complexity of the code

--- U1172227Q2.cpp
+++ U1172227Q2.cpp
@@ -7,6 +7,9 @@ void simpleFunc() {
     int temp2 = 0;
     int num = 0;
     int n = 100;
+    /*
+     *  The time complexity of the code below is of O(n^3)
+     */
     for (int c = 1; c <= n; c++) {
         temp1 = 0;
         for (int b = 1; b <= n; b++) {
@@ -16,6 +19,10 @@ void simpleFunc() {
         }
         num += temp1;
     }
+
+    /*
+     *  The time complexity of the code below is the same as of the above code block, O(n^3)
+     */
     for (int c = 1; c <= n; c++) {
         temp2 = 0;
         for (int b = 1; b <= n; b++) {
@@ -25,4 +32,8 @@ void simpleFunc() {
         }
         num += temp2;
     }
+
+    /*
+     *  And when we do the following summation O(n^3) + O(n^3) the result is simplified to O(n^3)
+     */
 }
