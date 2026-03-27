7103b81140e43322833ff58bf315282be5daf999
dimadeloseros1 <dimadeloseros1@gmail.com>
2026-03-27 20:21:14 +1000

    Feat: Implementl function to read the file

--- U11722227Q3.cpp
+++ U11722227Q3.cpp
@@ -1,3 +1,22 @@
-//
-// Created by dimad on 27/03/2026.
-//
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+void readingFile();
+
+int main() {
+    readingFile();
+}
+
+void readingFile() {
+    // string inputFile;
+    fstream inputFile("RF1.txt");
+    string readFile;
+    while (getline(inputFile, readFile)) {
+        cout << readFile << endl;
+    }
+
+    inputFile.close();
+}
+
